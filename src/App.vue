83e910922af2174d4a50351a0ec34e62b2f57f73
muhammadBilal924 <mbilal60924@gmail.com>
2022-02-18 18:43:39 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <h1>Form with checkbox and radio button in vue js</h1>
+    <sign-up />
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import SignUp from './components/SignUp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    SignUp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+
---
+++ src/components/SignUp.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+
+ <input type="text" placeholder="enter yuor name" v-model="signup.username" /><br><br>
+ <input type="password" placeholder="enter yuor password" v-model="signup.password"/><br><br>
+  <p>Choose Hobbies</p>
+ <input type="checkbox" id="circket" value="circket" v-model="signup.password" />
+ <label for="circket">Cricket
+ </label>
+  <p>Choose Hobbies</p>
+  <input type="checkbox" id="football" value="football" v-model="signup.hobbies" />
+ <label for="football">Football
+ </label>
+ <p>Select Gender</p>
+  <input type="radio" id="male" value="male" v-model="signup.gender" />
+ <label for="male">Male
+ </label>
+ <p>Select Gender</p>
+  <input type="radio" id="female" value="female" v-model="signup.gender" /><br><br>
+ <label for="female">Female
+ </label><br><br>
+ <button v-on:click="signupUser">Sign Up</button>
+    </div>
+</template>
+
+<script>
+    export default {
+     name:"SignUp",
+      data(){
+          return{
+              signup:{
+              username:null,
+              password:null,
+              hobbies:[],
+              gender:null
+              }
+          }
+      },
+      methods:{
+       signupUser(){
+           consol.console.warn(this.signup)
+       }
+      }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
